5b19fecce1e6e4c375a4e85d576c0364f605a9ba
Kaitlin <smith.kaitlin@gmail.com>
2022-12-01 21:17:14 -0500

    Solutions for both parts of Day 1

---
+++ solutions/2022_01.py
@@ -0,0 +1,28 @@
+dataset = '../puzzle_data/2022_01.txt'
+
+highest_calories = 0
+current_count = 0
+top_3_calorie_counts = []
+
+with open(dataset, 'r') as file:
+    for line in file:
+        if line != '\n':
+            current_count = current_count + int(line)
+        else:
+            top_3_calorie_counts.append(current_count)
+            top_3_calorie_counts.sort(reverse=True)
+
+            current_count = 0
+
+            if len(top_3_calorie_counts) > 3:
+                top_3_calorie_counts.pop(3)
+
+top_3_calorie_counts.append(current_count)
+top_3_calorie_counts.sort(reverse=True)
+top_3_calorie_counts.pop(3)
+
+print(top_3_calorie_counts)
+print(top_3_calorie_counts[0])
+print(sum(top_3_calorie_counts))
+
+
