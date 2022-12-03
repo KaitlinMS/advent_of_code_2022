2b616ae84baa817f865a4851bf2a122aa1b1ee8b
Kaitlin <smith.kaitlin@gmail.com>
2022-12-02 23:05:16 -0500

    Day 2 part 2

--- solutions/2022_02.py
+++ solutions/2022_02.py
@@ -1,10 +1,9 @@
 # A = rock
 # B = paper
 # C = scissors
-
-# X = rock
-# Y = paper
-# Z = scissors
+# X = rock / lose
+# Y = paper / draw
+# Z = scissors / win
 
 # Rock = 1 pt
 # Paper = 2 pt
@@ -19,18 +18,35 @@ loss_conditions = ['A Z', 'B X', 'C Y']
 draw_conditions = ['A X', 'B Y', 'C Z']
 
 dataset = '../puzzle_data/2022_02.txt'
-score = 0
+part_1_score = 0
+part_2_score = 0
 
 with open(dataset) as file:
     for index, line in enumerate(file):
         stripped_line = line.strip()
         opponent_move, my_move = stripped_line.split()
 
+        # PART 1
         if stripped_line in win_conditions:
-            score = score + 6 + point_values[my_move]
+            part_1_score = part_1_score + 6 + point_values[my_move]
         elif stripped_line in loss_conditions:
-            score = score + point_values[my_move]
+            part_1_score = part_1_score + point_values[my_move]
         elif stripped_line in draw_conditions:
-            score = score + 3 + point_values[my_move]
+            part_1_score = part_1_score + 3 + point_values[my_move]
 
-print(score)
+        # PART 2
+        if my_move == 'X':  # I need to lose
+            move_set = [i for i in loss_conditions if opponent_move in i]
+            opponent_shape, my_shape = move_set[0].split()
+            part_2_score = part_2_score + point_values[my_shape]
+        elif my_move == 'Y':  # I need to draw
+            move_set = [i for i in draw_conditions if opponent_move in i]
+            opponent_shape, my_shape = move_set[0].split()
+            part_2_score = part_2_score + 3 + point_values[my_shape]
+        elif my_move == 'Z':  # I need to win
+            move_set = [i for i in win_conditions if opponent_move in i]
+            opponent_shape, my_shape = move_set[0].split()
+            part_2_score = part_2_score + 6 + point_values[my_shape]
+
+print(part_1_score)
+print(part_2_score)
