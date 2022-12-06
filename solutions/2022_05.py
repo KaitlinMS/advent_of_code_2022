a1a00008decf3950d12104d3fe287fd5a543e6a9
Kaitlin <smith.kaitlin@gmail.com>
2022-12-06 13:26:25 -0500

    Day 5
    WIP. Will come back to this later.

--- solutions/2022_05.py
+++ solutions/2022_05.py
@@ -1,50 +1,51 @@
-import numpy
-
 dataset = '../puzzle_data/2022_05_sample.txt'
 stacks = []
-columns = 3  # I can easily tell this from looking at the data
+rotated_stacks = []
+columns = 3
 
-with open(dataset) as file:
-    for index, line in enumerate(file):
-        # Ignore the column labels and the empty line
-        # But make sure to check for empty lines first to avoid a crash
-        if line == '\n' or line.strip()[0].isdigit():
-            continue
 
-        elif line.startswith('move'):
-            instructions = line.split()  # [move, X, from, Y, to, Z]
-            moves = int(instructions[1])
-            current_row = 0
+def print_stacks(stack):
+    for row in range(len(stack)):
+        print(stack[row])
 
-            # Subtracting 1 because arrays are zero indexed
-            start_col = int(instructions[3]) - 1
-            end_col = int(instructions[5]) - 1
 
-            print('Instruction:')
-            print(line)
+def build_stacks(data):
+    with open(data) as file:
+        for index, line in enumerate(file):
+            if line.strip()[0].isdigit():
+                # We've hit the column labels, and we can now stop reading the file
+                break
 
-            # range starts at 0 and ends at moves-1
-            for move in range(moves):
-                # Find the top of the column
-                if stacks[current_row][end_col] == '[*]':
-                    current_row += 1
-                    continue
+            # I'm using [-] to denote an empty spot
+            # I'm making sure that we don't lose any column information by adding empty spots
+            stacks.append(line.replace('   ', '[-]').split())
 
-                moving_item = stacks[current_row][start_col]
-                stacks[current_row][start_col] = '[*]'
-
-                stacks[current_row][end_col] = moving_item
-
-        else:
-            # I'm using [*] to denote an empty spot
-            # Then I'm splitting each item in the stack so that this becomes a 2D array
-            stacks.append(line.replace('   ', '[*]').split())
-
-            # We need to pad out the array with empty spaces on the right side now
+            # Let's also add empty space padding to the end of the lists to ensure they're all the same size
             while len(stacks[index]) < columns:
-                stacks[index].append('[*]')
+                stacks[index].append('[-]')
 
-        print('Stacks:')
-        print(numpy.matrix(stacks))
-        print()
+    # Now we need to rotate this sucker so that we can easily append and pop items
+        rotated_stacks = stacks
+        for row in rotated_stacks:
+            print(row)
 
+
+            for item in reversed(row):
+                print(item)
+
+
+
+
+def make_moves(data):
+    with open(data) as file:
+        for index, line in enumerate(file):
+            # We can ignore everything that doesn't start with move
+            if not line.startswith('move'):
+                continue
+            else:
+                instructions = line.split()  # [move, X, from, Y, to, Z]
+
+
+reversed_stacks = build_stacks(dataset)
+print('Initial setup:')
+# print_stacks(reversed_stacks)
